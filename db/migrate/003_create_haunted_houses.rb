54ede9dd43351304c1829f0eeed223494a55fb7d
Koray Ozkal <koryozkal@gmail.com>
2020-07-23 07:10:52 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,23 +1,18 @@
-class CreateCostumes< ActiveRecord::Migration[4.2]
+class CreateHauntedHouse< ActiveRecord::Migration[4.2]
   def change
     create_table :dogs do |t|
       t.string :name
       t.string :location
       t.string :theme
       t.integer :price
-      t.string
-      t.string
-      t.string
-      t.string
+      t.string :family_friendly
+      t.string :opening_date
+      t.string :closing_date
+      t.string :long_description
       t.string
 
 
-:theme
-:price
-whether they're family friendly or not
-opening date
-closing date
-long description
+
 
   end
   end
