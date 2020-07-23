4c1651de573619da2672b79e90e063328037a963
Koray Ozkal <koryozkal@gmail.com>
2020-07-23 07:06:41 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,3 +6,11 @@ class CreateCostumes< ActiveRecord::Migration[4.2]
     end
   end
 end
+
+name
+location
+number of costumes, or "costume inventory"
+number of employees
+whether or not it's still in business
+opening time
+closing time
