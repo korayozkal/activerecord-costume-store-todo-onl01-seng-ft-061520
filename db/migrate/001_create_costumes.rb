594bf685a56da4b4f2ba7b2ba7474046edf0ad0d
Koray Ozkal <koryozkal@gmail.com>
2020-07-23 07:43:25 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,4 +1,4 @@
-class CreateCostumes< ActiveRecord::Migration[4.2]
+class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
       create_table :costumes do |t|
         t.string :name
@@ -6,7 +6,7 @@ class CreateCostumes< ActiveRecord::Migration[4.2]
         t.string :size
         t.string :image_url
         t.timestamps
-    end
+      end
   end
 end
 
