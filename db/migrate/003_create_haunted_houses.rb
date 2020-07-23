9176a5deb6324dbc15111abac83c6936ce04eb96
Koray Ozkal <koryozkal@gmail.com>
2020-07-23 07:09:17 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,7 +2,23 @@ class CreateCostumes< ActiveRecord::Migration[4.2]
   def change
     create_table :dogs do |t|
       t.string :name
-      t.string :breed
-    end
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.string
+      t.string
+      t.string
+      t.string
+      t.string
+
+
+:theme
+:price
+whether they're family friendly or not
+opening date
+closing date
+long description
+
+  end
   end
 end
