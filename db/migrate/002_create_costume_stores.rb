b75daa3edbedc8d378cd52fa6c1062edf5a6f4d1
Koray Ozkal <koryozkal@gmail.com>
2020-07-23 06:57:54 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,8 @@
-# Create your costume_stores migration here
+class CreateCostumes< ActiveRecord::Migration[4.2]
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+  end
+end
