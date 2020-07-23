95a06c77aa23dc48bf29c478cc6c5d8bc0bbee6d
Koray Ozkal <koryozkal@gmail.com>
2020-07-23 07:30:05 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,14 +1,14 @@
 class CreateCostumes< ActiveRecord::Migration[4.2]
   def change
-    create_table :dogs do |t|
+    create_table :costume_stores do |t|
       t.string :name
       t.string :breed
     end
   end
 end
 
-name
-location
+:name
+:location
 number of costumes, or "costume inventory"
 number of employees
 whether or not it's still in business
