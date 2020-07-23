0c40f67f03a360d8fc173f4c7ed67dfd9608f1c5
Koray Ozkal <koryozkal@gmail.com>
2020-07-23 07:31:22 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,8 +1,11 @@
 class CreateCostumes< ActiveRecord::Migration[4.2]
   def change
-    create_table :dogs do |t|
-      t.string :name
-      t.string :breed
+      create_table :costumes do |t|
+        t.string :name
+        t.integer :price
+        t.string :size
+        t.string :image_url
+        t.timestamps
     end
   end
 end
