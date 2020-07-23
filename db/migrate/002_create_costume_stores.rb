2cdd4a7cf7a7bb81690312b693402744b87413c2
Koray Ozkal <koryozkal@gmail.com>
2020-07-23 07:43:51 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,4 +1,4 @@
-class CreateCostumeStores< ActiveRecord::Migration[4.2]
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |t|
       t.string :name
@@ -6,9 +6,9 @@ class CreateCostumeStores< ActiveRecord::Migration[4.2]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.datetime :opening_time
-      t.datetime :closing_time
-    end 
-  end 
-end 
+      t.string :opening_time
+      t.string :closing_time
+    end
+  end
+end
  
